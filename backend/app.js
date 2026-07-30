838bdf5c4794190db055020e83a2ff6031dda5f9
mubashir sikandar <mubashirsikandarpatoli@gmail.com>
2026-07-30 15:04:01 -0700

    Update CORS

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,10 @@ const app_route=require('./routes/applications_route');
 require('dotenv').config()   
 const app=express();
 app.use(express.json());
-app.use(cors());
 
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://skill-swipe-tiu3.vercel.app']
+}));
 
 
 
